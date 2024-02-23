cf3d51bca3c41c9c9686ab9e8a8c654b51e47ff4
Sudhist Kumar <121331538+ersudhist@users.noreply.github.com>
2024-02-23 12:03:58 +0530

    being-abroad

--- Institute-Management-System/src/test/java/com/ims/InstituteManagementSystemApplicationTests.java
+++ Institute-Management-System/src/test/java/com/ims/InstituteManagementSystemApplicationTests.java
@@ -1,5 +1,11 @@
 package com.ims;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +15,42 @@ class InstituteManagementSystemApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	 @Test
+	 void testExampleService() {
+	        // Mocking dependencies
+	        Dependency dependency = mock(Dependency.class);
+	        when(dependency.getData()).thenReturn("Mocked data");
+
+	        // Creating an instance of the class to be tested, injecting the mocked dependency
+	        ExampleService exampleService = new ExampleService(dependency);
+
+	        // Calling the method under test
+	        String result = exampleService.processData();
+
+	        // Verifying the behavior
+	        verify(dependency, times(1)).getData();
+
+	        // Asserting the result using AssertJ
+	        assertThat(result).isEqualTo("Mocked data");
+	    }
+	}
+
+	// ExampleService class to be tested
+	class ExampleService {
+	    private Dependency dependency;
+
+	    public ExampleService(Dependency dependency) {
+	        this.dependency = dependency;
+	    }
+
+	    public String processData() {
+	        return dependency.getData();
+	    }
+	}
+
+	// Example dependency interface
+	interface Dependency {
+	    String getData();
 
 }
